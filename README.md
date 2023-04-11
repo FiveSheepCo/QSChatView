4e31479b79b4fe897935a97e52fc40f5f1e26282
SplittyDev <splittydev@gmail.com>
2023-04-12 01:07:20 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# QSChatView
+
+A description of this package.
---
+++ Tests/QSChatViewTests/QSChatViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import QSChatView
+
+final class QSChatViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(QSChatView().text, "Hello, World!")
+    }
+}
