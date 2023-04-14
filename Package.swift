186bbe7a70c36e1747ea3dcf40fd43c43ca0c8c6
SplittyDev <splittydev@gmail.com>
2023-04-14 02:36:18 +0200

    Add mac catalyst target

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "QSChatView",
     platforms: [
         .iOS(.v15),
-        .macOS(.v12)
+        .macOS(.v12),
+        .macCatalyst(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
